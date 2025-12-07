63087a7173545c47498f3f04a45b93594ed29fed
saidmounaim <saidmounaim00@gmail.com>
2025-12-07 09:16:02 +0100

    Upgrade Next.js

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { toast } from "sonner";
-import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
